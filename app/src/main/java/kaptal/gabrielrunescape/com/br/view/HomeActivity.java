473d0aaa35a29b9cd4e4a75971ca4763940722ef
Gabriel Filipe <gabrielfilipe@mail.ru>
2016-12-08 22:48:31 -0200

    Inserido e realizada algumas mudanças quanto ao design:   - Adicionado um spinner na tela inicial;   - ListView para exibição das informações;   - Inserido icones e string para uso na aplicação

--- app/src/main/java/kaptal/gabrielrunescape/com/br/view/HomeActivity.java
+++ app/src/main/java/kaptal/gabrielrunescape/com/br/view/HomeActivity.java
@@ -1,7 +1,12 @@
 package kaptal.gabrielrunescape.com.br.view;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import kaptal.gabrielrunescape.com.br.R;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -24,5 +29,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    /**
+     * Sobreescreve o método extendido <strong>onResume()</strong>.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * Sobreescreve o método extendido <strong>onCreateOptionsMenu()</strong>.
+     * @param menu Toolbar da aplicação.
+     * @return Verdadeiro somente.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_home, menu);
+        return true;
     }
 }
